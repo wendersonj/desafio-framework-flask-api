36e1031d438877f36fd0b6609058eb57f5adc475
exwendjs <X4vier@23>
2022-01-21 19:28:46 -0300

    fix: using correct conversion to camel case

--- projeto/utils/camel_model.py
+++ projeto/utils/camel_model.py
@@ -3,17 +3,13 @@ from re import sub
 from pydantic import BaseModel
 
 
-def camel_to_snake(name):
-    # https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
-    name = sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
-
-
-def to_camel(string):
-    return camel_to_snake(string)
+def camel_case(s):
+    #https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-96.php
+    s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+    return ''.join([s[0].lower(), s[1:]])
 
 
 class CamelModel(BaseModel):
     class Config:
-        alias_generator = to_camel
+        alias_generator = camel_case
         allow_population_by_field_name = True
