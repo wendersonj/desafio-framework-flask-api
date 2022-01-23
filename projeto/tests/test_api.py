702c4e8c1f85ba133aca5996de640db6a9968d65
exwendjs <X4vier@23>
2022-01-23 09:52:21 -0300

    refact: improved test

--- projeto/tests/test_api.py
+++ projeto/tests/test_api.py
@@ -1,9 +1,9 @@
-from projeto.app import app
+from projeto.index import app
 
 
 def test_success_response():
     response = app.test_client().get('/get_first_five_todo')
 
     assert response.status_code == 200
-    assert 'todo_list' in response.json.keys()
+    assert response.json.get('todo_list', None) is not None
     assert len(response.json['todo_list']) == 5
