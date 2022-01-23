02d044831f85edfe38f65e212652bc1b772e598c
exwendjs <X4vier@23>
2022-01-23 09:52:40 -0300

    refact: organized file

--- projeto/models/todo.py
+++ projeto/models/todo.py
@@ -1,19 +1,12 @@
 from typing import List
-
-from pydantic import root_validator
-
 from projeto.utils.camel_model import CamelModel
+
 
 class ToDo(CamelModel):
     id: int
     user_id: str
     title: str
     completed: bool
-
-    @root_validator()
-    def test(cls, values):
-        # print(values)
-        return values
 
 
 class ToDoList(CamelModel):
